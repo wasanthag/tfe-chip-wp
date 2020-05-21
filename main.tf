f7f54a96be01035a22a44493eaee1eb4fadde26a
wasanthag <gamagew@wasantha-fedora.lan>
2020-05-21 10:13:30 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  alias  = "us-west-1"
+  region = "us-west-1"
+}
+
+
+resource "aws_rds_cluster" "default" {
+  provider            = aws.us-west-1
+  cluster_identifier  = "wp-demo"
+  engine              = "aurora-mysql"
+  engine_version      = "5.7.mysql_aurora.2.03.2"
+  availability_zones  = ["us-west-1b", "us-west-1c"]
+  database_name       = "wpdb"
+  master_username     = "admin"
+  master_password     = "chip2020"
+  skip_final_snapshot = true
+}
+
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  provider           = aws.us-west-1
+  count              = 2
+  identifier         = "aurora-cluster-demo-${count.index}"
+  engine             = "aurora-mysql"
+  engine_version     = "5.7.mysql_aurora.2.03.2"
+  cluster_identifier = aws_rds_cluster.default.id
+  instance_class     = "db.t2.small"
+}
